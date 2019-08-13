1ee9e971f4ccebc3fb3ffbaef36cd4c6d421b64d
Kashiful Haque <37375667+kashifulhaque@users.noreply.github.com>
2019-08-14 01:39:03 +0530

    A list displays courses

--- app/src/main/java/saif/nidhi/udemyclone/DashboardActivity.java
+++ app/src/main/java/saif/nidhi/udemyclone/DashboardActivity.java
@@ -4,19 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import saif.nidhi.udemyclone.models.Course;
+import saif.nidhi.udemyclone.models.CourseAdapter;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class DashboardActivity extends AppCompatActivity {
 
     // Widgets
-    private TextView mUsername;
     private Button mLogout;
+    private RecyclerView mRecyclerView;
+    private CourseAdapter adapter;
+
+    // Model Variables
+    private ArrayList<String> courseName = new ArrayList<>();
+    private ArrayList<String> courseCode = new ArrayList<>();
+    private ArrayList<String> courseAuthor = new ArrayList<>();
+    private ArrayList<String> courseDifficulty = new ArrayList<>();
+    private ArrayList<Long> courseVideos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +44,7 @@ public class DashboardActivity extends AppCompatActivity {
 
         initializeWidgets();
         checkAuth();
+        getFirebaseData();
 
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,14 +58,53 @@ public class DashboardActivity extends AppCompatActivity {
         });
     }
 
+    private void getFirebaseData() {
+
+        FirebaseDatabase.getInstance().getReference().child(getString(R.string.dbnode_courses))
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                            fetchData(singleSnapshot);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+    }
+
+    private void fetchData(DataSnapshot singleSnapshot) {
+        Course course = singleSnapshot.getValue(Course.class);
+
+        if (course != null) {
+            courseName.add(course.getName());
+            courseAuthor.add(course.getAuthor());
+            courseDifficulty.add(course.getDifficulty());
+            courseCode.add(course.getCourse_code());
+            courseVideos.add(course.getVideos());
+
+            initialize();
+        }
+    }
+
+    private void initialize() {
+        mRecyclerView = findViewById(R.id.rvCourseList);
+        adapter = new CourseAdapter(this, courseName, courseAuthor, courseDifficulty, courseCode, courseVideos);
+        mRecyclerView.setAdapter(adapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
     private void initializeWidgets() {
-        mUsername = findViewById(R.id.tvUsername);
         mLogout = findViewById(R.id.btnLogout);
     }
 
     private void checkAuth() {
         if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            mUsername.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+            Toast.makeText(this, "Logged In.", Toast.LENGTH_SHORT).show();
         }
     }
 }
